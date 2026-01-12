2c8942ec5e901c6c7c75ef5767c9bdc998b180a1
Carlos <carloshlzansavio@gmail.com>
2026-01-12 20:06:24 +0000

    Update jobs/gold.py

--- jobs/gold.py
+++ jobs/gold.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 # %%
 dbutils.widgets.text("silver_path", "/Volumes/workspace/silver/covid/", "Output Path")
-dbutils.widgets.text("gold_path", "/Volumes/workspace/gold/covid/", "Output Path")
+dbutils.widgets.text("gold_path", "/Volumes/workspace/gold/", "Output Path")
 
 # %%
 # Retrieve the values
